fa5c372e8fbf6d453cfb89ba39c7fd34cdce17d8
shahriar-shakil <toshahriarshakil@gmail.com>
2020-07-13 22:52:21 +0600

    Updates

---
+++ precache-manifest.960e932e98f1baf03c6d5659fa15afe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fded2c68c60816aa6f505a157f0b7bd5",
+    "url": "/MiNigh/index.html"
+  },
+  {
+    "revision": "70e2c7f02ab64ac1351e",
+    "url": "/MiNigh/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ca6679d449fc1b1eea44",
+    "url": "/MiNigh/static/css/main.94dcba47.chunk.css"
+  },
+  {
+    "revision": "70e2c7f02ab64ac1351e",
+    "url": "/MiNigh/static/js/2.e06e0eda.chunk.js"
+  },
+  {
+    "revision": "ca6679d449fc1b1eea44",
+    "url": "/MiNigh/static/js/main.fbf34588.chunk.js"
+  },
+  {
+    "revision": "50d75fc9aa261e53d5fd",
+    "url": "/MiNigh/static/js/runtime-main.e0573215.js"
+  }
+]);
